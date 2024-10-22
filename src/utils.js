2386185fbb9b9ff103c0efeac6e970921f1e6b86
yaxyobekb <pubgn9642@gmail.com>
2024-10-22 20:52:33 +0500

    add utils.js file

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+// Filter data by Date
+export const filterByDate = (data, startDate, endDate) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  return data.filter((item) => {
+    const date = new Date(item[0]);
+    return date >= start && date <= end;
+  });
+};
