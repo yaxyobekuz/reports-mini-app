aa5d6f16a1f57ccec0164453df8a09168cf2ac20
yaxyobekb <pubgn9642@gmail.com>
2024-10-31 11:36:56 +0500

    add report types data

---
+++ src/data/reportTypes.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 0,
+    label: "Kunlik",
+    value: "day",
+  },
+  {
+    id: 1,
+    label: "Haftalik",
+    value: "week",
+  },
+  {
+    id: 2,
+    label: "Oylik",
+    value: "month",
+  },
+  {
+    id: 3,
+    label: "Barcha",
+    value: "all",
+  },
+];
