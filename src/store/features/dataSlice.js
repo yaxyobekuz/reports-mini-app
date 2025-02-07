aab3ef39fbc6b2d30188e2c28ba57b442b75df6c
yaxyobekb <pubgn9642@gmail.com>
2025-02-07 23:49:23 +0500

    add data2 structure to dataSlice for enhanced state management

--- src/store/features/dataSlice.js
+++ src/store/features/dataSlice.js
@@ -9,14 +9,21 @@ export const dataSlice = createSlice({
       week: null,
       month: null,
     },
+    data2: {
+      day: null,
+      all: null,
+      week: null,
+      month: null,
+    },
     error: false,
     loader: true,
   },
 
   reducers: {
     updateData: (state, action) => {
-      const { data, type } = action.payload;
+      const { data, data2, type } = action.payload;
       state.data[type] = data;
+      state.data2[type] = data2;
     },
 
     updateLoader: (state, action) => {
