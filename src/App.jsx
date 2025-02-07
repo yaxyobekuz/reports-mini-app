39f6f4820d494a9c0708f0f0fe87478fe967cb91
yaxyobekb <pubgn9642@gmail.com>
2025-02-07 11:41:27 +0500

    add Update routing to include Add, Reports, and Settings components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import React, { useEffect } from "react";
-
 // Router
 import {
   Route,
@@ -8,45 +6,24 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 
+// Pages
+import Add from "./pages/Add";
+import Reports from "./pages/Reports";
+import Settings from "./pages/Settings";
+import useTelegram from "./hooks/useTelegram";
+
 // Layouts
 import MainLayout from "./layouts/MainLayout";
 
-// Pages
-import Home from "./pages/Home";
-
-// Redux
-import {
-  updateData,
-  updateError,
-  updateLoader,
-} from "./store/features/dataSlice";
-import { useDispatch } from "react-redux";
-import axios from "axios";
-
 const App = () => {
-  const today = new Date();
-  const dispatch = useDispatch();
-  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-  const apiUrl = `${apiBaseUrl}?start=${new Date("01-01-1900")}&end=${today}`;
-
-  // Fetch data from API
-  const fetchData = () => {
-    // Add loader
-    dispatch(updateLoader(true));
-
-    axios
-      .get(apiUrl)
-      .then((res) => dispatch(updateData(res.data)))
-      .catch(() => dispatch(updateError(true)))
-      .finally(() => dispatch(updateLoader(false)));
-  };
-
-  useEffect(() => fetchData(), []);
+  const { id } = useTelegram()?.user || {};
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/:p?" element={<MainLayout />}>
-        <Route index element={<Home />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Add />} />
+        <Route path="/hisobotlar" element={<Reports />} />
+        <Route path="/sozlamalar" element={<Settings />} />
       </Route>
     )
   );
